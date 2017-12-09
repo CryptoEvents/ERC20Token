b29f5bb711d4e4f1b76ebda847fedc932e5e71fa
Andrey <systerr@gmail.com>
2017-12-09 14:21:44 +0100

    create lottery

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -12,6 +12,10 @@ contract ERC20Token is MintableToken {
         require(_owner != address(0));
         owner = _owner;
     }
+
+    function createLottery() public {
+        // todo for feature see road map or whitepaper (not exist right now)
+    }
     /**
      * Modification - owner only
      * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
